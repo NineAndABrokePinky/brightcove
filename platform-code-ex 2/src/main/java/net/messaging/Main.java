951a8cc4f834053d010c15508570378b09717b6b
joseph.edwards <joseph.edwards@adesa.com>
2018-07-11 20:53:55 -0400

    Added comments for the Main class

--- platform-code-ex 2/src/main/java/net/messaging/Main.java
+++ platform-code-ex 2/src/main/java/net/messaging/Main.java
@@ -1,28 +1,48 @@
 package net.messaging;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
 	private static Writer network;
 	private static Writer console;
 
+	/**
+	 * Setter for network
+	 * @param network -  StringWriter
+	 */
 	public static void setNetwork(Writer network) {
 		Main.network = network;
 	}
-
+	
+	/**
+	 * Setter for console
+	 * @param console -  StringWriter
+	 */
 	public static void setConsole(Writer console) {
 		Main.console = console;
 	}
 	
+	/**
+	 * Getter for network
+	 */
 	public static Writer getNetwork() {
 		return network;
 	}
 	
+	/**
+	 * Getter for Console
+	 */
 	public static Writer getConsole() {
 		return console;
 	}
 
+	/**
+	 * This will construct the email that will be written to the StringBuffer
+	 * @param emailAddress - String of the emailAddress to be sent to
+	 * @param body - String of the message that will be sent in the email
+	 * @return
+	 * @throws IOException - In case of StringWriter not being initialized
+	 */
 	public static String createEmail(String emailAddress, String body) throws IOException {
 		String email = "";
 		email += Constants.CONNECTION_HEADER + '\n';
